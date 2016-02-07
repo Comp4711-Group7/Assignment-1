54d555a9eba87170a495f1264b344d959aa53914
Mike Lewis <mike_lewis_@hotmail.com>
2016-02-07 15:03:13 -0800

    added assets folder, created base _template, and created main page

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -33,8 +33,9 @@ class Application extends CI_Controller {
 	 */
 	function render()
 	{
-		$this->data['menubar'] = $this->parser->parse('_menubar', $this->config->item('menu_choices'), true);
-		$this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
+		// These two lines are from Jim's example.
+		//$this->data['menubar'] = $this->parser->parse('_menubar', $this->config->item('menu_choices'), true);
+		//$this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
 
 		// finally, build the browser page!
 		$this->data['data'] = &$this->data;
