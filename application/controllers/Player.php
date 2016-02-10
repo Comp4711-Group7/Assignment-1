5504418f986b04401f19d2c33f9f2747a691169f
Mike Lewis <mike_lewis_@hotmail.com>
2016-02-09 20:11:50 -0800

    added comments to player controller

--- application/controllers/Player.php
+++ application/controllers/Player.php
@@ -11,29 +11,29 @@ class Player extends Application {
 
     public function index()
     {
-        $this->data['players'] = $this->getPlayers();
+        $this->data['players'] = $this->getPlayers();  // When the page loads, list all players in db
         $this->data['pagebody'] = 'players';	// this is the view we want shown
         $this->render();
     }
 
+    /**
+     * @return array of all of the players in the database
+     */
     public function getPlayers() {
         $query = $this->db->get('players');
-
-        //foreach ($query->result() as $row)
-        //{
-        //    echo $row->Player;
-        //}
-
         return $query->result_array();
     }
 
+    /**
+     * @param $name Takes a name and returns a view with that player's profile
+     */
     public function getPlayerInfo($name) {
+        // Query gets player with specific name
         $this->db->select('*');
         $this->db->from('players');
         $this->db->where('Player', $name);
         $query = $this->db->get();
 
-        echo "hello";
         $this->data['playerprofile'] = $query->result_array();
         $this->data['pagebody'] = 'playerprofile';	// this is the view we want shown
         $this->render();
