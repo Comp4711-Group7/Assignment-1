ee68e4660fa082a34f1bf21eba415f0e32c97ada
Mike Lewis <mike_lewis_@hotmail.com>
2016-02-11 13:32:35 -0800

    fixed player links

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,7 +53,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['stocks'] = 'stocks';
 $route['players'] = 'player';
-$route['players/(:any)'] = 'player/getPlayerInfo/$1';
+$route['players/(:any)'] = 'players/getPlayerInfo/$1';
 $route['stocks/(:any)'] = 'stocks/getStockInfo/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
