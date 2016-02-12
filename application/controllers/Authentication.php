5005cde0eaf877b55dccade34ae9d9bf342b79a7
Jessica <jessicasaini@hotmail.com>
2016-02-12 15:30:51 -0800

    Adding comments to my work

--- application/controllers/Authentication.php
+++ application/controllers/Authentication.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+/* this controller is used for logging in and logging out, sets session info */
 class Authentication extends Application {
 
 
@@ -15,29 +15,22 @@ class Authentication extends Application {
 	{
 		
 	}
-        
+        /*function  is called when the user clicks login */
         public function login(){
             $newdata = array(
                    'username'  => 'Henry',
                    'email'     => 'henry@gmail.com',
                    'logged_in' => TRUE
                );
-
-                $this->session->set_userdata($newdata);
-               // $this->data['user_info'] = $newdata;
-               //$this->data['pagebody'] = 'homepage';	// this is the view we want shown
-		//$this->render(); 
-                
+               $this->session->set_userdata($newdata);
                redirect('/', 'refresh');
         }
-        
+        /*function is called when user clicks logout */
         public function logout(){
             $array_items = array('username' => '', 'email' => '', 'logged_in' => FALSE);
             $this->session->unset_userdata($array_items);
             $this->data['user_info'] = $array_items;
             $this->session->sess_destroy();
-            //$this->data['pagebody'] = 'homepage';	// this is the view we want shown
-            //$this->render();  
             redirect('/', 'refresh');
         }
 }
