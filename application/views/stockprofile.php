0b4c9c80ca57e45a3860e4c9548db73b592bc9b1
Jox <joxched@gmail.com>
2016-03-30 11:00:40 -0700

    fix error in stock profile view

--- application/views/stockprofile.php
+++ application/views/stockprofile.php
@@ -1,41 +1,37 @@
-<hr>
-
 <br>
-<div style="text-align: center">{playerdropdown}</div>
+<div style="text-align: center">{stocksdropdown}</div>
 
-<h3>Transactions</h3>
-
+<h4>Movements</h4>
 
 <table style="width:100%">
     <tr>
         <th>DATE</th>
-        <th>STOCK</th>
         <th>ACTION</th>
+        <th>AMOUNT</th>
+    </tr>
+    {stockmovements}
+    <tr>
+        <td>{Datetime}</td>
+        <td>{Action}</td>
+        <td>{Amount}</td>
+    </tr>
+    {/stockmovements}
+</table>
+
+<h4>Transactions</h4>
+<table style="width:100%">
+    <tr>
+        <th>DATE</th>
+        <th>PLAYER</th>
+        <th>TRANSACTION</th>
         <th>QUANTITY</th>
     </tr>
-    {playerprofile}
+    {stocktransactions}
     <tr>
         <td>{DateTime}</td>
-        <td>{Stock}</td>
+        <td>{Player}</td>
         <td>{Trans}</td>
         <td>{Quantity}</td>
     </tr>
-    {/playerprofile}
+    {/stocktransactions}
 </table>
-
-</br>
-
-<h3>Player Holdings</h3>
-<table style="width:100%">
-    <tr>
-        <th>STOCK</th>
-        <th>TOTAL</th>
-    </tr>
-    {playerholdings}
-    <tr>
-        <td>{stock}</td>
-        <td>{quantity}</td>
-    </tr>
-    {/playerholdings}
-</table>
-
