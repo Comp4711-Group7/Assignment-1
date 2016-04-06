f9ba52da48e1ceebb3623a96e195e9b40dd4d88b
Mike Lewis <mike_lewis_@hotmail.com>
2016-04-06 10:14:05 -0700

    added management page and controller

---
+++ application/controllers/Management.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Management extends Application
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index() {
+        $this->load->model('player');
+        $this->data['pagebody'] = 'management';	// this is the view we want shown
+        $this->data['title'] = 'Agent Management';
+        $this->render();
+    }
+}
