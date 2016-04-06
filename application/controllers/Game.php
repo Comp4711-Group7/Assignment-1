79198d2bf8f7029265969161807c4caa6e4492ab
Mike Lewis <mike_lewis_@hotmail.com>
2016-04-06 10:46:00 -0700

    function pulls down round and state from bsx server

--- application/controllers/Game.php
+++ application/controllers/Game.php
@@ -3,9 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Game extends Application
 {
+    public $xml = null;
+    public $state;
+    public $round;
+
     function __construct()
     {
         parent::__construct();
+        $this->xml = simplexml_load_file('http://bsx.jlparry.com/status');
+        $this->getStatus();
     }
 
     public function index() {
@@ -13,5 +19,10 @@ class Game extends Application
         $this->data['pagebody'] = 'game';	// this is the view we want shown
         $this->data['title'] = 'Game Page';
         $this->render();
+    }
+
+    public function getStatus() {
+        $this->round = $this->xml->round;
+        $this->state = $this->xml->state;
     }
 }
