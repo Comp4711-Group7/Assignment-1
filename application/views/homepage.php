863bc0357cd74a0050b2fad20d005516b2c3e891
Jox <joxched@gmail.com>
2016-04-10 14:07:10 -0700

    fix typo error on AuthController

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,35 +1,63 @@
+<!---->
+<!--<h2 style="text-align:center">Stocks</h2>-->
+<!--<table style="width:100%">-->
+<!--    <tr>-->
+<!--        <th>CODE</th>-->
+<!--        <th>NAME</th>-->
+<!--        <th>CATEGORY</th>-->
+<!--        <th>VALUE</th>-->
+<!--    </tr>-->
+<!--    {stocks}-->
+<!--    <tr>-->
+<!--        <td>{Code}</td>-->
+<!--        <td><a href="stocks/{Code}">{Name}</a></td>-->
+<!--        <td>{Category}</td>-->
+<!--        <td>{Value}</td>-->
+<!--    </tr>-->
+<!--    {/stocks}-->
+<!--</table>-->
+<!---->
+<!---->
+<!--<h2 style="text-align:center">Players</h2>-->
+<!--<table style="width:100%">-->
+<!--    <tr>-->
+<!--        <th>PLAYER</th>-->
+<!--        <th>CASH</th>-->
+<!--        <th>EQUITY</th>-->
+<!--    </tr>-->
+<!--    {players}-->
+<!--    <tr>-->
+<!--        <td><a href="players/{Player}">{Player}</a></td>-->
+<!--        <td>{Cash}</td>-->
+<!--        <td>{Equity}</td>-->
+<!--    </tr>-->
+<!--    {/players}-->
+<!--</table>-->
 
-<h2 style="text-align:center">Stocks</h2>
-<table style="width:100%">
-    <tr>
-        <th>CODE</th>
-        <th>NAME</th>
-        <th>CATEGORY</th>
-        <th>VALUE</th>
-    </tr>
-    {stocks}
-    <tr>
-        <td>{Code}</td>
-        <td><a href="stocks/{Code}">{Name}</a></td>
-        <td>{Category}</td>
-        <td>{Value}</td>
-    </tr>
-    {/stocks}
-</table>
 
-
-<h2 style="text-align:center">Players</h2>
-<table style="width:100%">
-    <tr>
-        <th>PLAYER</th>
-        <th>CASH</th>
-        <th>EQUITY</th>
-    </tr>
-    {players}
-    <tr>
-        <td><a href="players/{Player}">{Player}</a></td>
-        <td>{Cash}</td>
-        <td>{Equity}</td>
-    </tr>
-    {/players}
-</table>
+<div class="row">
+    <div class="col-md-8">
+        <div class="row stocks">
+            STOCKS
+            <img src="http://placehold.it/650x300">
+        </div>
+        <div class="row stocks">
+            ACTIVE PLAYERS
+            <img src="http://placehold.it/650x300">
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="row stocks">
+            GAME STATUS
+            <img src="http://placehold.it/300x200">
+        </div>
+        <div class="row stocks">
+            RECENT STOCK MOVEMENTS
+            <img src="http://placehold.it/300x200">
+        </div>
+        <div class="row stocks">
+            RECENT TRANSACTIONS
+            <img src="http://placehold.it/300x200">
+        </div>
+    </div>
+</div>
