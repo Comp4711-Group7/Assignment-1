caa61ba770befd8ea233bde241f0b555f7c99999
Mike Lewis <mike_lewis_@hotmail.com>
2016-04-12 09:01:24 -0700

    removed some functions from game controller, as they have moved to the game model

--- application/controllers/Games.php
+++ application/controllers/Games.php
@@ -13,8 +13,6 @@ class Games extends Application
         parent::__construct();
         $this->restrict(array(ROLE_USER));
         $this->load->model('game');
-        //$this->getStatus();
-        //$this->checkRound();
     }
 
     public function index() {
@@ -22,36 +20,5 @@ class Games extends Application
         $this->data['title'] = 'Game Page';
         $this->data["stocks"] = $this->game->getStocks();
         $this->render();
-    }
-
-    // Populates data memebers with current status from BSX server
-    public function getStatus() {
-        $this->xml = simplexml_load_file('http://bsx.jlparry.com/status');
-        $this->round = $this->xml->round;
-        $this->state = $this->xml->state;
-        $this->countdown = $this->xml->countdown;
-    }
-
-    // Checks what state the server is in, then takes an action depending on that state
-    public function checkRound() {
-        // Ensure the status is up to date before preforming any action
-        $this->getStatus();
-        $state = $this->getState();
-
-        if($state == 0) {
-            echo "Game is not running";
-        } elseif($state == 1) {
-            echo "Game is in setup mode";
-        } elseif($state == 2) {
-            echo "Game is ready!";
-        } elseif($state == 3) {
-            echo "Game is active";
-        } else {
-            echo "The current round is over";
-        }
-    }
-
-    public function getState() {
-        return $this->state;
     }
 }
