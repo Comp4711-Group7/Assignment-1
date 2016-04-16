375088304ec95b1dce8ea57551931ec31a97898f
Jox <joxched@gmail.com>
2016-04-16 14:17:24 -0700

    fix extra / on url

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -17,8 +17,8 @@ class Welcome extends Application {
 		$this->data['stocks'] = $this->stock->getStocks(BSX_SERVER.'data/stocks');
 		$this->data['users'] = $this->user->getUsers();
 		$this->data['gameStatus'] = $this->game->getStatus();
-		$this->data['recentStockMovements'] = $this->stock->getMovements(BSX_SERVER.'/data/movement');
-		$this->data['recentStockTrans'] = $this->stock->getTransactions(BSX_SERVER.'/data/transactions');
+		$this->data['recentStockMovements'] = $this->stock->getMovements(BSX_SERVER.'data/movement');
+		$this->data['recentStockTrans'] = $this->stock->getTransactions(BSX_SERVER.'data/transactions');
 		$this->data['pagebody'] = 'homepage';	// this is the view we want shown
 		$this->render();
 	}
